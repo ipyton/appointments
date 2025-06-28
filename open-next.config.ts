116554144ce8f0c42ebc94bafb3fa8710036772a
ipyton <56996660+ipyton@users.noreply.github.com>
2025-06-28 14:34:40 +1000

    debug

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,5 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig();
+export default {
+    // 指定使用 cloudflare 类型构建（支持 Workers）
+    output: "standalone",
+    adapter: "cloudflare",
+  }
